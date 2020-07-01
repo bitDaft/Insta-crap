1f9fb70198726b2e0f1d3d924c5033a7a4fd0e2f
Tausif ali <ajaxhis@tutanota.com>
2020-07-01 11:25:43 +0530

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:"",
+  component : MainComponent
+},
+{
+  path:"bookmarks",
+  component : BookmarksComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
