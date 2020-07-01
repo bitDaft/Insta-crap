ed100046eff48b75e63f6190aa6f56e955f67562
Tausif ali <ajaxhis@tutanota.com>
2020-07-01 11:24:58 +0530

    added env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  postUrl:
+    'https://hiit.ria.rocks/videos_api/cdn/com.rstream.crafts?versionCode=40&lurl=Canvas%20painting%20ideas',
+  commentsUrl: 'http://cookbookrecipes.in/test.php',
+  commentsUrlD: 'http://localhost:4200/test.php',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  postUrl:
+    'https://hiit.ria.rocks/videos_api/cdn/com.rstream.crafts?versionCode=40&lurl=Canvas%20painting%20ideas',
+  commentsUrl: 'http://cookbookrecipes.in/test.php',
+  commentsUrlD: 'http://localhost:4200/test.php',
 };
 
 /*
